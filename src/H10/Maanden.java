8022842e74456eff2d67d99afa327c4e79cd037a
Partypig <cangel@roc-dev.com>
2016-10-10 14:44:36 +0200

    .

--- src/H10/Maanden.java
+++ src/H10/Maanden.java
@@ -3,78 +3,140 @@ package H10;
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.*;
 
 /**
  * Created by {Corne Angel} on 30-9-2016.
  */
 public class Maanden extends Applet {
 
-    TextField nummer;
-    int maand;
-    String s, tekst;
-    Label label;
+    //    maand
+    TextField Maandnummer;
+    Label maand;
+    Button B1;
+    int Maandint;
+    String Ms;
+    //    jaar
+    TextField Jaarnummer;
+    Label jaar;
+    int Jaarint;
+    String Js;
+    String Feb;
+
 
     @Override
     public void init() {
-        nummer = new TextField("",20);
-        label = new Label("Type het maandnummer en druk op enter");
-        nummer.addActionListener(new TekstvakListener());
-        tekst= "";
-        add(label);
-        add(nummer);
+        super.init();
+
+        ButtonListener bl = new ButtonListener();
+        AnotherClass ac = new AnotherClass();
+        Maandnummer = new TextField("", 5);
+        Maandnummer.addActionListener(bl);
+        maand = new Label("Maand");
+        B1 = new Button("Enter");
+        B1.addActionListener(bl);
+        B1.addActionListener(ac);
+        jaar = new Label("Jaar");
+        Jaarnummer = new TextField("", 5);
+        Jaarnummer.addActionListener(ac);
+
+        add(jaar);
+        add(Jaarnummer);
+
+        add(maand);
+        add(Maandnummer);
+        add(B1);
+        Ms="";
+        Js="";
+    }
+
+    class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if ( (Jaarint % 4 == 0 && !(Jaarint % 100 == 0)) ||
+                    Jaarint % 400 == 0 ) {
+
+                Feb = "28";
+            }
+            else {
+
+                Feb = "29";
+            }
+
+            Maandint = Integer.parseInt(Maandnummer.getText());
+            switch (Maandint) {
+                case 1:
+                    Ms = "Januari, 31 Dagen";
+                    break;
+                case 2:
+                    Ms = "Febuari, " +Feb+ " Dagen"; // moet aangepast worden tijdens schirkkeljaar
+                    break;
+                case 3:
+                    Ms = "Maart, 31 Dagen";
+                    break;
+                case 4:
+                    Ms = "April, 30 Dagen";
+                    break;
+                case 5:
+                    Ms = "Mei, 31 Dagen";
+                    break;
+                case 6:
+                    Ms = "Juni, 30 Dagen";
+                    break;
+                case 7:
+                    Ms = "Juli, 31 Dagen";
+                    break;
+                case 8:
+                    Ms = "Augustus, 31 Dagen";
+                    break;
+                case 9:
+                    Ms = "September, 30 Dagen";
+                    break;
+                case 10:
+                    Ms = "Oktober, 31 Dagen";
+                    break;
+                case 11:
+                    Ms = "November, 30 Dagen";
+                    break;
+                case 12:
+                    Ms = "December, 31 Dagen";
+                    break;
+                default:
+                    Ms = "Ongeldige maand!";
+                    break;
+            }
+            Maandnummer.setText("");
+            repaint();
+        }
+    }
+    class AnotherClass implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+
+            Jaarint =Integer.parseInt(Jaarnummer.getText());
+
+            if ( (Jaarint % 4 == 0 && !(Jaarint % 100 == 0)) ||
+                    Jaarint % 400 == 0 ) {
+                Js = ""+ Jaarint + " is een schrikkeljaar";
+                Feb = "28";
+            }
+            else {
+                Js = ""+ Jaarint + " is geen schrikkeljaar";
+                Feb = "29";
+            }
+            Jaarnummer.setText("");
+            repaint();
+        }
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawString(tekst,50,70);
-    }
+        super.paint(g);
 
-    class TekstvakListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            s= nummer.getText();
-            maand = Integer.parseInt( s);
-            switch(maand) {
-                case 1:
-                    tekst = "Januari, 31 dagen";
-                    break;
-                case 2:
-                    tekst = "Februari, 31 dagen";
-                    break;
-                case 3:
-                    tekst = "Maart, 31 dagen";
-                    break;
-                case 4:
-                    tekst = "April, 30 dagen";
-                    break;
-                case 5:
-                    tekst = "Mei, 31 dagen";
-                    break;
-                case 6:
-                    tekst = "Juni, 30 dagen";
-                    break;
-                case 7:
-                    tekst = "Juli, 31 dagen";
-                    break;
-                case 8:
-                    tekst = "Augustus, 31 dagen";
-                    break;
-                case 9:
-                    tekst = "September, 30 dagen";
-                    break;
-                case 10:
-                    tekst = "Oktober, 31 dagen";
-                    break;
-                case 11:
-                    tekst = "November, 30 dagen";
-                    break;
-                case 12:
-                    tekst = "December, 31 dagen";
-                    break;
-            }
-            repaint();
-
-        }
+        g.drawString(""+ Ms,80,50);
+        g.drawString(""+Js,80,80);
     }
 }
--- src/H10/PraktijkOpdrachtH10.java
+++ src/H10/PraktijkOpdrachtH10.java
@@ -1,7 +1,75 @@
 package H10;
 
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by {Corne Angel} on 6-10-2016.
  */
-public class PraktijkOpdrachtH10 {
+public class PraktijkOpdrachtH10 extends Applet {
+
+    TextField tekstveld;
+    Button knop;
+
+    double invoer;
+    double totaal;
+
+    double gemiddelde;
+
+    int aantalcijfers;
+
+    boolean geklikt;
+
+    @Override
+    public void init() {
+        tekstveld = new TextField("", 5);
+        add(tekstveld);
+
+        knop = new Button("Ok");
+        add(knop);
+        KnopListener kl = new KnopListener();
+        knop.addActionListener(kl);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int x = 50;
+        int y = 60;
+
+        if (geklikt) {
+            if ( invoer < 5.5 ) {
+                g.drawString("Het cijfer is onvoldoende ", x,y);
+            } else {
+                g.drawString("Het cijfer is voldoende ", x,y);
+            }
+
+            y = y + 20;
+            g.drawString("Gemiddelde" + gemiddelde, x,y);
+        }
+
+
+    }
+
+    class KnopListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstveld.getText();
+            invoer = Double.parseDouble(s);
+
+            totaal += invoer;
+            // of totaal = totaal + invoer
+
+            aantalcijfers++;
+            // is aantalcijfers = aantaalcijfers + 1;
+
+            gemiddelde = totaal / aantalcijfers;
+            geklikt=true;
+
+            repaint();
+
+        }
+    }
 }
